7bb0dc33f43572e16f38a710dd30a4298191e630
irvinzato <multizato@hotmail.com>
2022-09-06 23:42:28 -0500

    Anotaciones PrePersist y PreUpdate para clase entity, super utiles para registros de fechas

--- src/main/java/org/rivera/hibernateapp/entity/Cliente.java
+++ src/main/java/org/rivera/hibernateapp/entity/Cliente.java
@@ -2,12 +2,15 @@ package org.rivera.hibernateapp.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "clientes")   //Nombre de tabla sql
 public class Cliente {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)   //Indio que es AI
+  @GeneratedValue(strategy = GenerationType.IDENTITY)   //Indico que es AI
   private Long id;
 
   @Column(name = "nombre")    //Como no se llaman igual que la tabla se lo indico
@@ -19,6 +22,13 @@ public class Cliente {
   @Column(name = "forma_pago")
   private String wayToPay;
 
+  //Columnas añadidas en práctica de eventos del ciclo de vida
+  @Column(name = "creado_en")
+  private LocalDateTime createIn;
+
+  @Column(name = "editado_en")
+  private LocalDateTime editedIn;
+
   // ¡Siempre debe haber un constructor vacío! para que JPA pueda instancear la clase
   public Cliente() {
   }
@@ -26,6 +36,19 @@ public class Cliente {
   public Cliente(String name, String lastName) {
     this.name = name;
     this.lastName = lastName;
+  }
+
+  //EVENTOS DE CICLO DE VIDA - UNA FUNCIÓN BASTANTE UTIL ES USARLOS PARA FECHAS DE CUANDO SE CREA ALGÚN REGISTRO O SE EDITA
+  @PrePersist
+  public void prePersist() {
+    System.out.println("Inicializar algo antes del PERSIST()");
+    this.createIn = LocalDateTime.now();    //Pongo fecha cuando es creado
+  }
+
+  @PreUpdate
+  public void preUpdate() {
+    System.out.println("Inicializar algo antes del UPDATE");
+    this.editedIn = LocalDateTime.now();    //Pongo fecha cuando es editado
   }
 
   public Long getId() {
@@ -60,12 +83,30 @@ public class Cliente {
     this.wayToPay = wayToPay;
   }
 
+  public LocalDateTime getCreateIn() {
+    return createIn;
+  }
+
+  public void setCreateIn(LocalDateTime createIn) {
+    this.createIn = createIn;
+  }
+
+  public LocalDateTime getEditedIn() {
+    return editedIn;
+  }
+
+  public void setEditedIn(LocalDateTime editedIn) {
+    this.editedIn = editedIn;
+  }
+
   @Override
   public String toString() {
     return "Cliente " +
             "id=" + id +
             ", name='" + name +
             ", lastName='" + lastName +
-            ", wayToPay='" + wayToPay;
+            ", wayToPay='" + wayToPay +
+            ", creado en = " + createIn +
+            ", editado en = " + editedIn;
   }
 }
